5da715fa645a282ab7b981a170bf70f78b9f828b
grace <justgrace69@gmail.com>
2020-04-06 20:03:03 +0300

    save

---
+++ src/js/script.js
@@ -0,0 +1,28 @@
+// let answer = [],
+//    question = [
+//       "сколько лет?",
+//       "сколько вес?",
+//       "сколько рост?",
+//    ];
+
+
+// // for (let i = 0; i < question.length; i++) {
+// //    answer[i] = prompt(question[i], '');
+// // };
+
+
+// // let i = 0;
+// // while (i < question.length) {
+// //    answer[i] = prompt(question[i], '');
+// //    i++;
+// // }
+
+// let i = 0;
+// do {
+//    answer[i] = prompt(question[i], '');
+//    i++;
+// } while (i < question.length);
+
+// console.log(answer);
+
+// document.write(answer);
